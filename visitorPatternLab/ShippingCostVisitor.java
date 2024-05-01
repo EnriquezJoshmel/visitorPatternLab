44aa20f5add079687720b6f0a5f2d13ec2f65ea2
EnriquezJoshmel <142742663+EnriquezJoshmel@users.noreply.github.com>
2024-05-01 14:38:47 +0800

    Update ShippingCostVisitor.java

--- visitorPatternLab/ShippingCostVisitor.java
+++ visitorPatternLab/ShippingCostVisitor.java
@@ -1,9 +1,9 @@
 package visitorPatternLab;
 
 public interface ShippingCostVisitor {
-    public double visit(Chair chair, double distance);
+    public double visit1(Chair chair, double distance);
 
-    public double visit(Table table, double distance);
+    public double visit2(Table table, double distance);
 
-    public double visit(Sofa sofa, double distance);
+    public double visit3(Sofa sofa, double distance);
 }
